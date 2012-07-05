cf02ffb88256d14359b8f7aab0e5c8911e054399
Jeffrey Mullins <jmullins@30and30.com>
2012-07-05 16:39:21 -0400

    Adding pidfile_proc_up.sh which tests if the process identified in the specified pidfile is up and running.

---
+++ env/common/opt/tr/bin/pidfile_proc_up.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+usage()
+{
+    echo "Check if process is up for a given pidfile."
+    echo ""
+    echo "Exits with return code 0 if pidfile exists and"
+    echo "there exists a running process containing the"
+    echo "string 'name'."
+    echo "Otherwise exits with non-zero return code."
+    echo ""
+    echo "usage: `basename $0` <pidfile> <name>"
+    exit 2
+}
+
+if [ $# -ne 2 ]
+then
+    usage
+fi
+
+pidfile=$1
+name=$2
+result=1
+
+if [ -f $pidfile ]
+then
+    output=$(ps `cat $pidfile` |grep $name)
+    result=$?
+fi
+
+exit $result
+
